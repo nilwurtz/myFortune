dc3760ba5831bf764a45647c81ce0433b0f67421
ragnar1904 <apollo.oth@gmail.com>
2022-03-06 13:12:08 +0900

    set base path

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import preact from "@preact/preset-vite";
 import { join } from "path";
 
 export default defineConfig({
+  base: "/myFortune/",
   resolve: {
     alias: {
       "@/": join(__dirname, "./src/"),
