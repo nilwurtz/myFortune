08151c1facde79cb1b2b858369bdce7ebef5159f
ragnar1904 <apollo.oth@gmail.com>
2022-03-11 00:59:01 +0900

    :art:Show event dates!

---
+++ src/components/atoms/fullYear.tsx
@@ -0,0 +1,15 @@
+import { EventDate } from "@/lib/events";
+import { FunctionalComponent } from "preact";
+
+type Props = EventDate;
+
+export const FullYear: FunctionalComponent<Props> = (props) => {
+  return (
+    <span>
+      <span>{props.year}年</span>
+      <span>{props.month}月</span>
+      <span>{props.date}日</span>
+      <span>（{props.day}）</span>
+    </span>
+  );
+};
--- src/components/pages/index.tsx
+++ src/components/pages/index.tsx
@@ -1,10 +1,31 @@
+import { EVENTS } from "@/lib/events";
+import { initialMeetsState, meetsReducer } from "@/reducers/meets";
 import { FunctionComponent } from "preact";
+import { useEffect, useReducer } from "preact/hooks";
+import { FullYear } from "@/components/atoms/fullYear";
 
 export const IndexPage: FunctionComponent = () => {
+  const [meets, dispatch] = useReducer(meetsReducer, initialMeetsState);
+
+  useEffect(() => {
+    dispatch({ type: "init", dates: EVENTS["4th"].dates });
+  }, []);
+
   return (
     <div>
       <section>
-        <div className="flex justify-end flex-row py-2 px-4" />
+        <div className="py-8 px-4">
+          {meets.meets.map((meet) => (
+            <div>
+              <FullYear
+                year={meet.date.year}
+                month={meet.date.month}
+                day={meet.date.day}
+                date={meet.date.date}
+              />
+            </div>
+          ))}
+        </div>
       </section>
     </div>
   );
